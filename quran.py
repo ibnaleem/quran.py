0316ed31f1dc8d7f5954e4d7a42b11a67c6ca09f
ibnaleem <ibnaleem@outlook.com>
2023-06-01 18:43:22 +0100

    Returns a list of all chapter names in the Qur'an written in Arabic

--- quran.py
+++ quran.py
@@ -10,6 +10,7 @@ class Chapters:
         self.all_simples_order = []
         self.all_complex = []
         self.all_complex_order = []
+        self.arabic_name = []
 
 
     def all_simple(self):
@@ -101,3 +102,23 @@ class Chapters:
         else:
             print(f"The API is currently down. Response Code: {response.status_code}")
 
+    def all_arabic(self):
+        """Returns a list of all chapter names in the Qur'An in Arabic"""
+
+        url = 'https://api.quran.com/api/v4/chapters?language=en'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+
+            data = response.json()
+            dumped_data = json.dumps(data)
+
+            parsed_data = json.loads(dumped_data)
+
+            for chapter in parsed_data["chapters"]:
+                self.arabic_name.append(chapter["name_arabic"])
+
+            return self.arabic_name
+
+        else:
+            print(f"The API is currently down. Response Code: {response.status_code}")
