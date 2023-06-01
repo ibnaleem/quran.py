349a4d5f1b01e5b0d2e34197c639ee42ae688449
ibnaleem <ibnaleem@outlook.com>
2023-06-01 18:52:35 +0100

    Returns a list of all chapters revealed in Mecca (simple notation)

--- quran.py
+++ quran.py
@@ -12,6 +12,7 @@ class Chapters:
         self.all_complex_order = []
         self.arabic_name = []
         self.translated_name = []
+        self.mecca_simples = []
 
 
     def all_simple(self):
@@ -142,6 +143,30 @@ class Chapters:
                 self.translated_name.append(ch["name"])
 
             return self.translated_name
+
+        else:
+            print(f"The API is currently down. Response Code: {response.status_code}")
+
+    def all_mecca_simple(self):
+        """Returns a list of all chapters revealed in Mecca (simple notation)"""
+
+        url = 'https://api.quran.com/api/v4/chapters?language=en'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+
+            data = response.json()
+            dumped_data = json.dumps(data)
+
+            parsed_data = json.loads(dumped_data)
+
+            for chapter in parsed_data["chapters"]:
+                if chapter["revelation_place"] == "makkah":
+                    self.mecca_simples.append(chapter["name_simple"])
+                else:
+                    pass
+
+            return self.mecca_simples
 
         else:
             print(f"The API is currently down. Response Code: {response.status_code}")
